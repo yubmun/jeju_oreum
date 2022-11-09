556f136a84cf7934ec930d6d3feb88f55dbf7ffc
junyub.lee <dlwnsduq23@gmail.com>
2022-11-09 20:43:39 +0900

    api 불러오기

---
+++ index.js
@@ -0,0 +1,68 @@
+// const marker = () => {
+//   fetch(
+//     "https://api.odcloud.kr/api/15096996/v1/uddi:6738a90c-ec96-4245-a187-9528cea62904?page=1&perPage=10&serviceKey=3MCBWEYPV4%2BY4Un8XqdBpFBiaGQKGsEVpC1HIK1DCoHqjNlhaUGcwjBIJGDYeTaTOiG4GKJorKXpGpfNpOEjhQ%3D%3D"
+//   )
+//     .then((r) => r.json())
+//     .then((r) => console.log(r))
+// }
+
+
+
+window.initMap = function () {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 33.3616658, lng: 126.5204118 },
+    zoom: 10,
+  });
+
+  fetch("https://api.odcloud.kr/api/15096996/v1/uddi:6738a90c-ec96-4245-a187-9528cea62904?page=1&perPage=90&serviceKey=3MCBWEYPV4%2BY4Un8XqdBpFBiaGQKGsEVpC1HIK1DCoHqjNlhaUGcwjBIJGDYeTaTOiG4GKJorKXpGpfNpOEjhQ%3D%3D")
+  .then((r) => r.json())
+  .then((r) => {
+    // console.log(r.data);
+    const infowindow = new google.maps.InfoWindow();
+
+    r.data.forEach((x) => {
+      let lat = parseFloat(x['위도']);
+      let lng = parseFloat(x['경도']);
+      let name = x['오름명'];
+      let content = x['설명'];
+
+      new google.maps.Circle({
+        strokeColor: "blue",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "blue",
+        fillOpacity: 0.35,
+        map,
+        center: {lat, lng},
+        radius: 600,
+      });
+
+      const mark = new google.maps.Marker({
+        position: {lat, lng},
+        name,
+        map: map,
+      })
+      mark.addListener("click", ()=>{
+        infowindow.setContent(`${name}\n ${content}`);
+        infowindow.open({
+          anchor: mark,
+          map,
+        })
+      })
+
+    })
+  })
+
+  
+  // const bounds = new google.maps.LatLngBounds();
+
+  // marker.forEach(({Name, lat, lng})=>{
+  //   const mark = new google.maps.Marker({
+  //     position: {lat, lng},
+  //     Name,
+  //     map: map,
+  //   });
+  //   bounds.extend(mark.position);
+  // });
+  // map.fitBounds(bounds);
+};
